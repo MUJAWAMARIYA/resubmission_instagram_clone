db1821daea36a4f815f2d0619044b88556e14acd
priscilleumurerwa <priscille.umurerwa@gmail.com>
2019-09-09 21:43:41 +0000

    instagram app error correction of page error, image size error and some other

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   
   resources :favorites
   resources :profiles
-
+  resources :posts
   resources :posts do
     resources :comments
   end
 
   devise_for :users, controllers: { sessions: "users/sessions" }
   root 'posts#index' 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
